c319112b1dfd86bbac1553c0d2a084e56d69fb59
Intetras <77957619+Intetras@users.noreply.github.com>
2023-05-19 16:57:37 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 //% weight=30 icon="\uf1b9" color=#000080 block="KAGA:bit-Drive"
 //% groups=['テスト1', 'テスト2']
 namespace kagabitdrive {
+
+    //% blockId=aaaaa
+    //% block="aaa"
+    export function aaa(): void { }
+
 }
