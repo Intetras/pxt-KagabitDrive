d5d1d1804f14395474d27d00d3957b1f24bdfdbc
Intetras <77957619+Intetras@users.noreply.github.com>
2023-05-19 16:53:10 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 //% weight=30 icon="\uf1b9" color=#000080 block="KAGA:bit-Drive"
-//% groups=['DCモーター', 'sサーボモーター']
+//% groups=['テスト1', 'テスト2']
 namespace kagabitdrive {
-    //% group="Test"
+    //% group="テスト1"
     //% blockId=Test
     //% block="テスト"
-    export function Test() {}
+    export function Test1() {
+
+    }
 }
